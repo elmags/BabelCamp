f020d3b3b308e119e6b12bef02821692bf10396d
elmags <e.gallardo@alumnos.upm.es>
2022-03-02 13:27:42 +0100

    Add clase resta

---
+++ Ejercicio1_Git/src/pGit/Resta.java
@@ -0,0 +1,14 @@
+package pGit;
+
+public class Resta {
+	private static int acumulado = 0;
+	
+	public static int resta(int x, int y) {
+		return x - y;
+	}
+	
+	public static int restaAcumulada(int x) {
+		acumulado = resta(acumulado, x);
+		return acumulado;
+	}
+}
