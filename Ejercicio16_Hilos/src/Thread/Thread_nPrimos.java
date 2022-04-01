de3c2424d5184846c76f3f096158ec9c725ea002
elmags <e.gallardo@alumnos.upm.es>
2022-04-01 13:43:15 +0200

    Add Ejercicio16_Hilos

---
+++ Ejercicio16_Hilos/src/Thread/Thread_nPrimos.java
@@ -0,0 +1,31 @@
+package Thread;
+
+public class Thread_nPrimos implements Runnable {
+	
+	private int numero;
+	
+	public Thread_nPrimos(int numero) {
+		this.numero = numero;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(esPrimo(numero));
+	}
+	
+	private boolean existeDivisor (int a, int b, int n) {
+		boolean resultado = false;
+	    int i = a;
+	    while (i <= b && !resultado){
+	      if (n % i == 0)
+	        resultado = true;
+	      else
+	        i = i+1;
+	    } 
+	    return resultado;
+	}
+	
+	private boolean esPrimo(int n) {
+		return !existeDivisor (2, n-1, n);
+	}
+}
